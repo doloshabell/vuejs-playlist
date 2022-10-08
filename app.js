d9b8d00481fd177d9e7853c65adf8ff67eeac2d7
doloshabell <habelldol@gmail.com>
2022-10-08 15:15:24 +0700

    done to follow video 2

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Shaun'
+    },
+    methods: {
+        greet: function(time) {
+            return 'Good ' + time + ', ' + this.name;
+        }
     }
 });
